5cae3464819c1a7d183e1fb87ec6ece860fce6d0
zhijianzhang <mrjzhang@tencent.com>
2018-05-14 22:50:51 +0800

    :fire: del:  remove files about mobx

--- src/App.js
+++ src/App.js
@@ -1,12 +1,9 @@
 import React, {Component} from 'react';
 import {Button} from 'antd';
-import {inject, observer} from 'mobx-react'
 import './App.css';
 
 // import {getElements} from './services/apis'
 
-@inject('pageStore')
-@observer
 class App extends Component {
 
   async componentDidMount() {
@@ -16,10 +13,14 @@ class App extends Component {
     // }
   }
   render() {
-    const {name} = this.props.pageStore
     return (
       <div>
-        Home
+        <div>input github account</div>
+        <div>commit nums</div>
+        <div>commit color</div>
+        <div>user icon</div>
+        <Button type="primary">刷新</Button>
+        <Button type="primary">修改账户</Button>
       </div>
     );
   }
